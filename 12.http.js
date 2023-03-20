051fead5557bf220e3f56ac2bd658f0418361f74
momokee2 <68141186+momokee2@users.noreply.github.com>
2023-03-20 17:30:41 +0800

    Update 12.http.js

--- 12.http.js
+++ 12.http.js
@@ -13,3 +13,16 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(5000)
+
+OR
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('HomePage');
+});
+
+server.listen(5000, () => {
+    console.log('Server running port 5000');
+})
